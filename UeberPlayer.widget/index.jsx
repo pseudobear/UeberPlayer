605b16bc6f8e439459155211388acc59d9efc4c9
Daniel Hernandez <tamdannyhdz@gmail.com>
2020-10-14 10:55:13 -0500

    Prepare release

--- UeberPlayer.widget/index.jsx
+++ UeberPlayer.widget/index.jsx
@@ -1,7 +1,7 @@
 
 import { styled, run } from "uebersicht";
 import getColors from './lib/getColors.js';
-const _version = '1.2.4';
+const _version = '1.3.0';
 
 /* CUSTOMIZATION (mess around here!)
 You may need to refresh the widget after changing these settings
