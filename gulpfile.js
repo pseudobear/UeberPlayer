bead0d0a87094cb024d9a6b30581e5a016e46a17
Daniel Hernandez <tamdannyhdz@gmail.com>
2020-12-15 01:02:10 -0600

    Update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,29 @@ const { src, dest, watch, series, lastRun } = require("gulp");
 const zip = require("gulp-zip");
 const path = require("path");
 
-const buildDist = () => src("./UeberPlayer.widget/**/*", {since: lastRun(buildDist)}).pipe(dest("./dist/UeberPlayer.widget"));
+const widgetLocation = `${process.env.HOME}/Library/Application Support/Übersicht/widgets`;
 
-const makeZip = () => src("./UeberPlayer.widget/**").pipe(zip("UeberPlayer.widget.zip")).pipe(dest("./"));
+const buildDist = () => (
+  src("./UeberPlayer.widget/**/*", { since: lastRun(buildDist) })
+    .pipe(dest("./dist/UeberPlayer.widget"))
+)
+
+const makeZip = () => (
+  src("./UeberPlayer.widget/**")
+    .pipe(zip("UeberPlayer.widget.zip"))
+    .pipe(dest("./"))
+)
+
+const apply = () => (
+  buildDist()
+    .pipe(dest(path.join(widgetLocation, 'UeberPlayer.widget')))
+)
 
 const dev = () => {
-  const widgetLocation = `${process.env.HOME}/Library/Application Support/Übersicht/widgets`;
-
-  watch("./UeberPlayer.widget/**/*", { ignoreInitial: false }, function watcher() {
-    return buildDist().pipe(dest(path.join(widgetLocation, "UeberPlayer.widget")));
-  });
+  watch('./UeberPlayer.widget/**/*', { ignoreInitial: false }, apply);
 }
 
 exports.default = buildDist;
-exports.release = series(exports.default, makeZip);
+exports.apply = apply;
 exports.dev = dev;
+exports.release = series(buildDist, makeZip);
